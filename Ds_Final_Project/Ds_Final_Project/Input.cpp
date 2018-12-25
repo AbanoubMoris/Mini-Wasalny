8ccfce1edde10ebb62b7dc7411529002b47d5076
abanoub abanoub <abanoub.goker@gmail.com>
2018-12-25 22:28:54 +0200

    12

--- Ds_Final_Project/Ds_Final_Project/Input.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include<iostream>
-#include"Graph.h"
-#include"Shape.h"
-using namespace std;
-int main() {
-
-	AppStart();
-	system("pause");
-}
--- Ds_Final_Project/Ds_Final_Project/Shape.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include<iostream>
-#include<conio.h>
-#include"Functions.h"
-void shape(int width, int height);
-void AppStart();
